3d1f3d173c728e63e69c9c920602256531856b7f
shadab <shad46243@gmail.com>
2024-07-04 20:28:57 +0530

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { ThemeProvider,createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ThemeProvider theme={createTheme()}>
+      <CssBaseline/>
+    <App />
+    </ThemeProvider>
+
+  </React.StrictMode>,
+)
